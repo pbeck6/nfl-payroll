db899b7994576ad27f1cdb9b0a0f8893a2e282df
Philip Beck <beckph@flip3.engr.oregonstate.edu>
2021-10-02 16:08:32 -0700

    Initialize files

---
+++ app.js
@@ -0,0 +1,41 @@
+// Express
+const express = require('express');
+const app = express();     
+PORT = 6661;
+// Database
+const db = require('./db-connector');
+
+app.get('/', function(req, res)
+    {
+        // Define our queries
+        query1 = 'DROP TABLE IF EXISTS diagnostic;';
+        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
+        query4 = 'SELECT * FROM diagnostic;';
+
+        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+
+        // DROP TABLE...
+        db.pool.query(query1, function (err, results, fields){
+
+            // CREATE TABLE...
+            db.pool.query(query2, function(err, results, fields){
+
+                // INSERT INTO...
+                db.pool.query(query3, function(err, results, fields){
+
+                    // SELECT *...
+                    db.pool.query(query4, function(err, results, fields){
+
+                        // Send the results to the browser
+                        let base = "<h1>MySQL Results:</h1>"
+                        res.send(base + JSON.stringify(results));
+                    });
+                });
+            });
+        });
+    });
+
+app.listen(PORT, function(){   // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
