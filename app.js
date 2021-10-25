b99ca0ccd7075fd7c24a6829d416ac3e749848d2
Philip Beck <beckph@flip3.engr.oregonstate.edu>
2021-10-24 23:04:49 -0700

    Clean-up files

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
 // Express
 const express = require('express');
 const app = express();     
-PORT = 6661;
+PORT = 6671;
 // Database
 const db = require('./db-connector');
 
 app.get('/', function(req, res)
-    {
-        // Define our queries
-        query1 = 'DROP TABLE IF EXISTS diagnostic;';
-        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
-        query4 = 'SELECT * FROM diagnostic;';
+    {   res.send('TEST WORKING');
+});
+    // {    
+    //     // Define our queries
+    //     query1 = 'DROP TABLE IF EXISTS diagnostic;';
+    //     query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+    //     query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
+    //     query4 = 'SELECT * FROM diagnostic;';
 
-        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+    //     // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
 
-        // DROP TABLE...
-        db.pool.query(query1, function (err, results, fields){
+    //     // DROP TABLE...
+    //     db.pool.query(query1, function (err, results, fields){
 
-            // CREATE TABLE...
-            db.pool.query(query2, function(err, results, fields){
+    //         // CREATE TABLE...
+    //         db.pool.query(query2, function(err, results, fields){
 
-                // INSERT INTO...
-                db.pool.query(query3, function(err, results, fields){
+    //             // INSERT INTO...
+    //             db.pool.query(query3, function(err, results, fields){
 
-                    // SELECT *...
-                    db.pool.query(query4, function(err, results, fields){
+    //                 // SELECT *...
+    //                 db.pool.query(query4, function(err, results, fields){
 
-                        // Send the results to the browser
-                        let base = "<h1>MySQL Results:</h1>"
-                        res.send(base + JSON.stringify(results));
-                    });
-                });
-            });
-        });
-    });
+    //                     // Send the results to the browser
+    //                     let base = "<h1>MySQL Results:</h1>"
+    //                     res.send(base + JSON.stringify(results));
+    //                 });
+    //             });
+    //         });
+    //     });
+    // });
 
-app.listen(PORT, function(){   // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+app.listen(PORT, function(){
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
