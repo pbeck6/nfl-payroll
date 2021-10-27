2ac1f80d9a16d210f3d12efce3803ef4738209a9
Philip Beck <beckph@flip3.engr.oregonstate.edu>
2021-10-27 02:37:42 -0700

    r

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AddPlayerPage from './pages/AddPlayerPage';
+import EditPlayerPage from './pages/EditPlayerPage';
 import {useState} from 'react';
 
+
 function App() {
-  const [] = useState();
+  const [playerToEdit, setplayerToEdit] = useState();
   return (
     <div className="App">
       <Router>
         <div className="App-header">
           <Route path="/" exact>
-            <HomePage />
+            <HomePage setPlayerToEdit={setPlayerToEdit}/>
+          </Route>
+          <Route path="/add-player">
+            <AddPlayerPage/>
+          </Route>
+          <Route path="/edit-player">
+            <EditPlayerPage playerToEdit={playerToEdit}/>
           </Route>
         </div>
       </Router>
