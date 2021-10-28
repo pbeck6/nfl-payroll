84d878b71167f5cabb0a70abdcd9912ae7301cc3
Philip Beck <beckph@flip3.engr.oregonstate.edu>
2021-10-28 01:03:54 -0700

    routes

--- app.js
+++ app.js
@@ -13,12 +13,32 @@ app.engine('.hbs', exphbs({                     // Create an instance of the han
 }));
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
-
-// Routes
+// Home Route
 app.get('/', function(req, res)
     {   res.render('home');
 });
+
+// Player Routes
+app.get('/player', function(req, res)
+    {   res.render('player/index');
+});
+
+// Team Routes
+app.get('/team', function(req, res)
+    {   res.render('team/index');
+});
+
+// Position Routes
+app.get('/position', function(req, res)
+    {   res.render('position/index');
+});
+
+// Coach Routes
+app.get('/coach', function(req, res)
+    {   res.render('coach/index');
+});
    
+// Listener
 app.listen(PORT, function(){
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
