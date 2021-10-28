6879fd7351eb2fe20d679b8bdddae5d09b5f5470
Philip Beck <beckph@flip3.engr.oregonstate.edu>
2021-10-27 23:33:19 -0700

    handlebars

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # NFL Payroll
 
+## - When working on FLIP server, need to use nvm to update Node to latest version, or at least v16.5.0. This is necessary for express-handlebars
+
 ## - app.js is missing a db-connector.js file, identical to the file for Task 1, that specifies the user's MariaDB login credentials, which should be stored and kept on local machine to separate credentials and port number
--- app.js
+++ app.js
@@ -6,8 +6,17 @@ const app = express();
 const db = require('./db-connector');
 const PORT = db.port;
 
+// Hnadlebars -> For displaying database results into HTML pages
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', exphbs({                     // Create an instance of the handlebars engine to process templates
+    extname: ".hbs"
+}));
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+
+// Routes
 app.get('/', function(req, res)
-    {   res.send('TEST WORKING');
+    {   res.render('index');
 });
     // {    
     //     // Define our queries
